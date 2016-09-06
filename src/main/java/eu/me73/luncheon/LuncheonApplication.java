84984d5dc5249aab27f6a54bef19b7ada3429220
Jozef Bacigal <jozef.bacigal@gmail.com>
2016-09-06 17:13:02 +0200

    initial commit

---
+++ src/main/java/eu/me73/luncheon/LuncheonApplication.java
@@ -0,0 +1,12 @@
+package eu.me73.luncheon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuncheonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuncheonApplication.class, args);
+	}
+}
---
+++ src/test/java/eu/me73/luncheon/LuncheonApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.me73.luncheon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LuncheonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
