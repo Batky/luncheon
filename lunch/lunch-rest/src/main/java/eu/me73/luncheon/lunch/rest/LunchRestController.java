5dc510d3d69feffb6415fb5d14dd7b6c7c1265c7
Jozef Bacigal <jozef.bacigal@me.com>
2016-09-17 09:26:01 +0200

    LUNCHEON-7 rest controller to add lunch

--- lunch/lunch-rest/src/main/java/eu/me73/luncheon/lunch/rest/LunchRestController.java
+++ lunch/lunch-rest/src/main/java/eu/me73/luncheon/lunch/rest/LunchRestController.java
@@ -4,10 +4,14 @@ import ch.qos.logback.classic.Logger;
 import eu.me73.luncheon.lunch.api.Lunch;
 import eu.me73.luncheon.lunch.api.LunchService;
 import java.util.Collection;
+import java.util.List;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -19,11 +23,25 @@ public class LunchRestController {
     LunchService lunchService;
 
     @RequestMapping(value = "/lunches", method = RequestMethod.GET, produces = "application/json")
-    public Collection<Lunch> getAllLunches(){
+    public Collection<Lunch> getAllLunches() {
         if (LOG.isDebugEnabled()) {
             LOG.debug("Rest request for all lunches.");
         }
         return lunchService.getAllLunches();
+    }
+
+    @RequestMapping(value = "/lunches/lunches", method = RequestMethod.POST, consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveLunches(@RequestBody List<Lunch> lunches) {
+        for (Lunch lunch : lunches) {
+            lunchService.save(lunch);
+        }
+    }
+
+    @RequestMapping(value = "/lunches/lunch", method = RequestMethod.POST, consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveLunch(@RequestBody Lunch lunch) {
+        lunchService.save(lunch);
     }
 
 }
